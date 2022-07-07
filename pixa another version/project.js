74d24e5ac080ccd184c153eda7a6857429a1f728
Amr Yasser <91434035+hu1kvic@users.noreply.github.com>
2022-07-07 05:55:04 +0200

    Add files via upload

---
+++ pixa another version/project.js
@@ -0,0 +1,7 @@
+AOS.init();
+var cursor1 = document.querySelector('.cursor1');
+var cursor2 = document.querySelector('.cursor2');
+document.addEventListener('mousemove', function(e) {
+    cursor1.style.cssText = cursor2.style.cssText = "left: " + e.clientX +
+        "px; top: " + e.clientY + "px;";
+});
